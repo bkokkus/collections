72940b41830d5486ee19098beb8a7aa2e5846b41
bkokkus <bekirkokkus@gmail.com>
2019-02-22 17:12:51 +0300

    added add method

--- src/Collections.php
+++ src/Collections.php
@@ -4,15 +4,22 @@ namespace Chestnut;
 
 class Collections
 {
-	protected $array;
+    protected $array;
 
-	public function __construct(array $arr = [])
-	{
-		$this->array = $arr;
-	}
+    public function __construct(array $arr = [])
+    {
+        $this->array = $arr;
+    }
 
-	public function toArray(): array
-	{
-		return $this->array;
-	}
+    public function toArray(): array
+    {
+        return $this->array;
+    }
+
+    public function add($key)
+    {
+        $this->array[] = $key;
+        return $this;
+    }
+
 }
