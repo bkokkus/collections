4d19012b86bbd51f7573b5223402d87e2f57e690
bkokkus <bekirkokkus@gmail.com>
2019-02-23 14:17:34 +0300

    implement ArrayAccess

--- src/Collections.php
+++ src/Collections.php
@@ -2,7 +2,7 @@
 
 namespace Chestnut;
 
-class Collections implements \Iterator
+class Collections implements \Iterator, \ArrayAccess
 {
     protected $array;
 
@@ -106,5 +106,29 @@ class Collections implements \Iterator
     public function __toString()
     {
         return $this->toJson(false);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if(is_null($offset)) {
+            $this->array[] = $value;
+        } else {
+            $this->array[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->array[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->array[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->array[$offset]) ? $this->array[$offset] : null;
     }
 }
